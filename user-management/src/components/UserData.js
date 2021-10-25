38ac0c0189bc82a4aa21c5f8923493fd7796e130
EmanAlrabie <emanalrubeie@gmail.com>
2021-10-25 22:07:47 +0300

    use Component life cycle

--- user-management/src/components/UserData.js
+++ user-management/src/components/UserData.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react'
 import './UserData.css'
 
 export default class UserData extends Component {
+    
+    updateUserInfo(data){
+        this.props.changeUserInfo(data)
+    }
+    
     render() {
+        
         return (
             <div className='user-data'>
-                <p >Name: Eman</p>
-                <p>Age: 24</p>
-                <p>Work: web developer</p>
-
+                <p >Name: {this.props.userInfo.name}</p>
+                <p>Age: {this.props.userInfo.age}</p>
+                
+                <button onClick={()=>{
+                        this.updateUserInfo({
+                            name: 'Mona',
+                            age:12,
+                        })
+                }}> Edit my info</button>
             </div>
         )
     }
--- user-management/src/pages/Users.js
+++ user-management/src/pages/Users.js
@@ -3,12 +3,45 @@ import UserData from '../components/UserData.js'
 import UserImage from '../components/UserImage.js'
 import './Users.css'
 export default class users extends Component {
+    
+    state={
+        numberOfUsers:2,
+        user1Info:{
+            name:'Eman',
+            age: 24,
+        },
+        user2Info:{
+            name:'Sara',
+            age: 21,
+        }
+    }
+
+    changeUserInfo(data){
+        this.setState({
+            ...this.state,
+            user1Info: data
+        })
+    }
+    componentDidMount(){
+        setTimeout(() => {
+            alert('Welcome to XXXX website')
+        }, 1000);
+        
+    }
+    componentDidUpdate(){
+             alert('your info update successfully')
+         }
+
+    componentWillUnmount(){
+            alert('The code changed')
+    }
+    
     render() {
         return (
             <div className='users'>
                 <div className='userItems'>
-                <UserImage/>
-                <UserData />
+                <UserImage />
+                <UserData userInfo={this.state.user1Info} changeUserInfo={this.changeUserInfo.bind(this)}/>
                 </div>
               
             </div>
