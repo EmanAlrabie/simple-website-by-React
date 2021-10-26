535d8434f2f9b2b66a3e1c415ad2482372f4fd4e
EmanAlrabie <emanalrubeie@gmail.com>
2021-10-26 13:53:23 +0300

    convert class based component to functional based component

--- user-management/src/components/UserData.js
+++ user-management/src/components/UserData.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react'
 import './UserData.css'
 
-export default class UserData extends Component {
+//class based Component
+// export default class UserData extends Component {
     
-    updateUserInfo(data){
-        this.props.changeUserInfo(data)
-    }
+//     updateUserInfo(data){
+//         this.props.changeUserInfo(data)
+//     }
     
-    render() {
+//     render() {
         
-        return (
-            <div className='user-data'>
-                <p >Name: {this.props.userInfo.name}</p>
-                <p>Age: {this.props.userInfo.age}</p>
+//         return (
+            // <div className='user-data'>
+            //     <p >Name: {this.props.userInfo.name}</p>
+            //     <p>Age: {this.props.userInfo.age}</p>
                 
-                <button onClick={()=>{
-                        this.updateUserInfo({
+            //     <button type="button" class="btn btn-info" onClick={()=>{
+            //             this.updateUserInfo({
+            //                 name: 'Mona',
+            //                 age:12,
+            //             })
+            //     }}> Edit my info</button>
+            // </div>
+//         )
+//     }
+// }
+
+
+//funcional based component
+
+function UserData(props) {
+    let {name, age} = props.userInfo
+
+    const updateUserInfo = (data)=>{
+        props.changeUserInfo(data)
+    }
+    return (
+        <div>
+            <div className='user-data'>
+                <p >Name: {name}</p>
+                <p>Age: {age}</p>
+                
+                <button type="button" class="btn btn-info" onClick={()=>{
+                        updateUserInfo({
                             name: 'Mona',
                             age:12,
                         })
                 }}> Edit my info</button>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
+export default UserData;
--- user-management/src/pages/Users.js
+++ user-management/src/pages/Users.js
@@ -1,50 +1,121 @@
-import React, { Component } from 'react'
+import React, { Fragment, useState, useEffect } from 'react';
 import UserData from '../components/UserData.js'
 import UserImage from '../components/UserImage.js'
 import './Users.css'
-export default class users extends Component {
-    
-    state={
-        numberOfUsers:2,
-        user1Info:{
-            name:'Eman',
-            age: 24,
-        },
-        user2Info:{
-            name:'Sara',
-            age: 21,
-        }
-    }
 
-    changeUserInfo(data){
-        this.setState({
-            ...this.state,
-            user1Info: data
-        })
+//class based component
+// export default class users extends Component {
+
+//     state={
+//         numberOfUsers:2,
+//         user1Info:{
+//             name:'Eman',
+//             age: 24,
+//         },
+//         user2Info:{
+//             name:'Sara',
+//             age: 21,
+//         }
+//     }
+
+//     changeUserInfo(data){
+//         this.setState({
+//             ...this.state,
+//             user1Info: data
+//         })
+//     }
+//     componentDidMount(){
+//         setTimeout(() => {
+//             alert('Welcome to XXXX website')
+//         }, 1000);
+
+//     }
+//     componentDidUpdate(){
+//              alert('your info update successfully')
+//          }
+
+//     componentWillUnmount(){
+//             alert('The code changed')
+//     }
+
+//     render() {
+//         return (
+//             <div className='users'>
+//                 <div className='userItems'>
+//                 <UserImage />
+//                 <UserData userInfo={this.state.user1Info} changeUserInfo={this.changeUserInfo.bind(this)}/>
+//                 </div>
+
+//             </div>
+//         )
+//     }
+// }
+
+
+//functional based component 
+
+function Users() {
+    const [numberOfUsers, setNumberOfUsers] = useState(2);
+    const [user1Info, setUser1Info] = 
+    useState({
+        name: 'Eman',
+        age: 24
+    });
+    const [user2Info, setUser2Info] = useState({
+        name: 'Sara',
+        age: 21,
+    });
+
+    const changeUserInfo = (data) => {
+        setUser1Info(data)
     }
-    componentDidMount(){
-        setTimeout(() => {
-            alert('Welcome to XXXX website')
-        }, 1000);
+    
+    //componentDidMount
+    useEffect(()=>{
+        alert('Welcome to XXXX website')
+    },[]);
+
+    //componentDidUpdate
+    useEffect(()=>{
+      if(user1Info.name == 'Eman'){
+          return
+      }
+      else{
+        alert('your info update successfully')
+      }
+       
+    });
+     
+    // componentWillUnmount
+    useEffect(()=>{
+        return(
+            alert('Unmount')
+        )
         
-    }
-    componentDidUpdate(){
-             alert('your info update successfully')
-         }
+    }, []);
 
-    componentWillUnmount(){
-            alert('The code changed')
-    }
-    
-    render() {
-        return (
+    return (
+        <Fragment>
+             
             <div className='users'>
                 <div className='userItems'>
-                <UserImage />
-                <UserData userInfo={this.state.user1Info} changeUserInfo={this.changeUserInfo.bind(this)}/>
+                <h1>
+                {numberOfUsers}
+                 active users
+            </h1> 
+            <br/>
+            <button type="button" class="btn btn-info"
+            onClick={()=>{
+                setNumberOfUsers(numberOfUsers+1)
+            }}>increment number of users</button>
+           <br/>
+           <br/>
+                    <UserImage />
+                    <UserData userInfo={user1Info} changeUserInfo={changeUserInfo} />
                 </div>
-              
             </div>
-        )
-    }
+        </Fragment>
+    )
 }
+
+export default Users;
